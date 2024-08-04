29eff3a616a270e5b2809805851352c3b19ac832
matteoNaN <matteonanni9897@gmail.com>
2024-08-04 13:25:37 +0200

    disegna la sfera con s ma ancora buggato
    disegna la sfera con s ma ancora buggato

--- src/Services/InputServices.ts
+++ src/Services/InputServices.ts
@@ -1,4 +1,13 @@
-import { Nullable, Scene, PointerInfo, PointerEventTypes, Vector3, MeshBuilder, LinesMesh } from "@babylonjs/core";
+import {
+  Nullable,
+  Scene,
+  PointerInfo,
+  PointerEventTypes,
+  Vector3,
+  MeshBuilder,
+  LinesMesh,
+  AbstractMesh,
+} from "@babylonjs/core";
 import eventEmitter from "../Events/misurazioneEventEmitter.ts";
 
 type ActionCallback = () => void;
@@ -7,11 +16,13 @@ type ActionCallback = () => void;
 export class InputService {
   private _actions: Map<string, ActionCallback> = new Map();
   private _sceneRef: Nullable<Scene> = null;
+
   private _firstPoint: Nullable<Vector3> = null;
   private _secondPoint: Nullable<Vector3> = null;
   private _lineMesh: Nullable<LinesMesh> = null;
   private _isMeasuringEnabled: boolean = false;
   private _drawSphereEnabled: boolean = false;
+  private _sphereRadius: Nullable<number> = null;
 
   constructor(sceneRef: Nullable<Scene>) {
     this._sceneRef = sceneRef;
@@ -22,24 +33,79 @@ export class InputService {
 
   private initActions() {
     // Inizializza le azioni qui
-    this._actions.set("POINTER_DOWN", () => this.handlePointerDown());
-    this._actions.set("POINTER_MOVE", () => this.handlePointerMove());
+    this._actions.set("POINTER_DOWN_MEASUREMENT", () =>
+      this.handleMeasurementPointerDown()
+    );
+    this._actions.set("POINTER_MOVE_MEASUREMENT", () =>
+      this.handleMeasuremetPointerMove()
+    );
+
+    this._actions.set("POINTER_DOWN_SPHERE", () =>
+      this.handleSpherePointerDown()
+    );
+    this._actions.set("POINTER_MOVE_SPHERE", () =>
+      this.handleSpherePointerMove()
+    );
+  }
+
+  private handleSpherePointerMove(): void {
+    
+  }
+  private handleSpherePointerDown(): void {
+    if (this._sceneRef) {
+      const pickResult = this._sceneRef.pick(
+        this._sceneRef.pointerX,
+        this._sceneRef.pointerY
+      );
+
+      if (pickResult && pickResult.hit) {
+        if (!this._firstPoint) {
+          this._firstPoint = pickResult.pickedPoint;
+        } else {
+          this._sphereRadius = Vector3.Distance(
+            this._firstPoint,
+            pickResult.pickedPoint!
+          );
+          console.log(`Radius set: ${this._sphereRadius}`);
+          const circle: AbstractMesh = MeshBuilder.CreateDisc("disc-mesh", {
+            radius: this._sphereRadius,
+            tessellation: 32,
+          },this._sceneRef);
+            
+          circle.position = this._firstPoint;
+          circle.rotation.x = Math.PI;
+          
+          
+
+        }
+      }
+    }
   }
 
   private setupPointerObservables() {
     if (this._sceneRef) {
       this._sceneRef.onPointerObservable.add((pointerInfo: PointerInfo) => {
-        if(this._isMeasuringEnabled){
-        switch (pointerInfo.type) {
-          case PointerEventTypes.POINTERDOWN:
-            this._executeAction("POINTER_DOWN");
-            break;
-          case PointerEventTypes.POINTERMOVE:
-            this._executeAction("POINTER_MOVE");
-            break;
+        if (this._isMeasuringEnabled) {
+          switch (pointerInfo.type) {
+            case PointerEventTypes.POINTERDOWN:
+              this._executeAction("POINTER_DOWN_MEASUREMENT");
+              break;
+            case PointerEventTypes.POINTERMOVE:
+              this._executeAction("POINTER_MOVE_MEASUREMENT");
+              break;
+          }
         }
-      }});
-    
+        if (this._drawSphereEnabled) {
+          switch (pointerInfo.type) {
+            case PointerEventTypes.POINTERDOWN:
+              this._executeAction("POINTER_DOWN_SPHERE");
+              break;
+            case PointerEventTypes.POINTERMOVE:
+              this._executeAction("POINTER_MOVE_SPHERE");
+              break;
+          }
+        }
+      });
     }
   }
 
@@ -47,15 +113,16 @@ export class InputService {
     window.addEventListener("keydown", (event) => {
       if (event.key === "m") {
         this._isMeasuringEnabled = !this._isMeasuringEnabled;
-        console.log(`Measuring mode: ${this._isMeasuringEnabled ? "enabled" : "disabled"}`);
+        console.log(
+          `Measuring mode: ${this._isMeasuringEnabled ? "enabled" : "disabled"}`
+        );
       }
-      if(event.key === "s"){
-        this._drawSphereEnabled =   !this._drawSphereEnabled
+      if (event.key === "s") {
+        this._drawSphereEnabled = !this._drawSphereEnabled;
+        console.log("disegnando la sfera");
       }
     });
-
   }
-  
 
   private _executeAction(action: string) {
     const callback = this._actions.get(action);
@@ -70,7 +137,7 @@ export class InputService {
     }
   }
 
-  private handlePointerDown() {
+  private handleMeasurementPointerDown() {
     if (this._sceneRef) {
       const pickResult = this._sceneRef.pick(
         this._sceneRef.pointerX,
@@ -92,23 +159,24 @@ export class InputService {
     }
   }
 
-  private handlePointerMove(): void {
-    if (this._firstPoint && !this._secondPoint && this._sceneRef)
-	{
-		const pickResult = this._sceneRef.pick(
-			this._sceneRef.pointerX,
-			this._sceneRef.pointerY
-		);
+  private handleMeasuremetPointerMove(): void {
+    if (this._firstPoint && !this._secondPoint && this._sceneRef) {
+      const pickResult = this._sceneRef.pick(
+        this._sceneRef.pointerX,
+        this._sceneRef.pointerY
+      );
 
-		if(this._lineMesh)
-		{
-			this._sceneRef.removeMesh(this._lineMesh)
-		}
+      if (this._lineMesh) {
+        this._sceneRef.removeMesh(this._lineMesh);
+      }
 
-		this._lineMesh = MeshBuilder.CreateLines("linea_misura", {
-			points:[this._firstPoint,pickResult.pickedPoint!],
-		},this._sceneRef)
-
+      this._lineMesh = MeshBuilder.CreateLines(
+        "linea_misura",
+        {
+          points: [this._firstPoint, pickResult.pickedPoint!],
+        },
+        this._sceneRef
+      );
     }
   }
 
@@ -116,7 +184,17 @@ export class InputService {
     if (this._firstPoint && this._secondPoint) {
       const distance = Vector3.Distance(this._firstPoint, this._secondPoint);
       console.log(`Distance between points: ${distance}`);
-      eventEmitter.emit('distanceCalculated', distance);
+      eventEmitter.emit("distanceCalculated", distance);
     }
+  }
+
+  private Cleanup() {
+     this._firstPoint= null;
+     this._secondPoint = null;
+     this._lineMesh= null;
+     this._isMeasuringEnabled= false;
+     this._drawSphereEnabled= false;
+     this._sphereRadius= null;
+
   }
 }
